afa407b0137cb0e7e499de73305eeca5a7a3996e
Mantas Palaima <palaima.mantas@gmail.com>
2015-10-04 13:31:56 +0300

    Fix build

--- debugdrawer-location/src/main/java/io/palaima/debugdrawer/location/LocationModule.java
+++ debugdrawer-location/src/main/java/io/palaima/debugdrawer/location/LocationModule.java
@@ -27,6 +27,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
